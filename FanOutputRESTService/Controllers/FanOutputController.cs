b6bdc4cbfe6fb3b5a8534de40c1edb581fce8808
Magnus Hansen <magnus131hansen@gmail.com>
2020-09-23 16:16:41 +0200

    Add project files.

---
+++ FanOutputRESTService/Controllers/FanOutputController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FanPutputModelLib;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FanOutputRESTService.Controllers
+{
+    [Route("api/FanOutputModel")]
+    [ApiController]
+    public class FanOutputController : ControllerBase
+    {
+        public static List<FanOutputModel> fanOutputReadings = new List<FanOutputModel>()
+        {
+            new FanOutputModel(1, "First Output", 15, 30),
+            new FanOutputModel(2, "Second Output", 18, 41),
+            new FanOutputModel(3, "Thrid Output", 24, 69),
+            new FanOutputModel(4, "Fourth Output", 22, 49),
+            new FanOutputModel(5, "Sixth Output", 20, 75),
+        };
+
+        // GET: api/<FanOutputController>
+        [HttpGet]
+        public IEnumerable<FanOutputModel> Get()
+        {
+            return fanOutputReadings;
+        }
+
+        // GET api/<FanOutputController>/5
+        [HttpGet("{id}")]
+        public FanOutputModel Get(int id)
+        {
+            return fanOutputReadings.Find(i => i.Id == id);
+        }
+
+        // POST api/<FanOutputController>
+        [HttpPost]
+        public void Post([FromBody] FanOutputModel value)
+        {
+            fanOutputReadings.Add(value);
+        }
+
+        // PUT api/<FanOutputController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] FanOutputModel value)
+        {
+            FanOutputModel fanOutput = Get(id);
+            if(fanOutput != null)
+            {
+                fanOutput.Id = value.Id;
+                fanOutput.Name = value.Name;
+                fanOutput.Temp = value.Temp;
+                fanOutput.Humidity = value.Humidity;
+            }
+        }
+
+        // DELETE api/<FanOutputController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FanOutputModel fanOutput = Get(id);
+            fanOutputReadings.Remove(fanOutput);
+        }
+    }
+}
---
+++ FanPutputModelLib/FanOutputModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FanPutputModelLib
+{
+    public class FanOutputModel
+    {
+        public FanOutputModel()
+        {
+            
+        }
+
+        public FanOutputModel(int id, string name, int temp, int humidity)
+        {
+            Id = id;
+            Name = name;
+            Temp = temp;
+            Humidity = humidity;
+        }
+
+        private string _name;
+        private int _temp;
+        private int _humidity;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Navn", "Navnet skal være minimum 2 karakterer lang");
+                }
+            }
+        }
+
+        public int Temp
+        {
+            get { return _temp; }
+            set
+            {
+                if (15 <= value && value <= 25)
+                {
+                    _temp = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Temp", "Temp skal være et tal mellem 15 og 25");
+                }
+            }
+        }
+
+        public int Humidity
+        {
+            get { return _humidity; }
+            set
+            {
+                if (30 <= value && value <= 80)
+                {
+                    _humidity = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Fugt", "Fugt skal være  et tal mellem 30 og 80");
+                }
+            }
+        }
+    }
+}
